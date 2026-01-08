12db8609b25eebe71cf9faa7cb6676fb1a349779
Willyan David <62126803+Redesu@users.noreply.github.com>
2026-01-08 13:05:22 -0300

    Updates search command description.

--- src/commands/stadium/search.ts
+++ src/commands/stadium/search.ts
@@ -6,12 +6,14 @@ export const search: Command = {
   data: new SlashCommandBuilder()
     .setName("search")
     .setDescription(
-      "Search across stadium items and powers. Use abbreviations, if multiple, use a comma."
+      "Search across stadium items and items stats. Use abbreviations (ap, wp, etc), if multiple, use a comma."
     )
     .addStringOption((option) =>
       option
         .setName("query")
-        .setDescription("The search query")
+        .setDescription(
+          "The search query, can contain multiple terms separated by a comma."
+        )
         .setRequired(true)
     ),
   async execute(interaction) {
